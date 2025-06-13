07009a5407e05aaac36a7023ce5ad2cecdefd1a6
Devansh Bansal <devanshbansal500@gmail.com>
2025-06-14 04:09:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
